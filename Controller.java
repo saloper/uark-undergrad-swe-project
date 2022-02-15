9ff06eb26752862a5ea7d0ad0b4aa55012f2c646
saloper <spencer.loper2018@gmail.com>
2022-02-15 15:12:11 -0600

    Initial Setup

---
+++ Controller.java
@@ -0,0 +1,20 @@
+//Controller File CSCE 3513 Team 6 Project
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Controller implements ActionListener{
+    //Class Variables
+    Model model;
+
+    //Constructor
+    public Controller(Model m){
+        this.model = m;
+    }
+    
+    //Action performed Method
+    public void actionPerformed(ActionEvent e){
+
+    }
+}
